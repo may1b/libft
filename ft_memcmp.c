f510df0f11243a8bb962f10b01fbae8f8ffca19e
May <magrass@student.42heilbronn.de>
2026-04-14 15:44:46 +0200

    not done, yet memcmp but something i did do in fact, this is true AND A FACT

---
+++ ft_memcmp.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: magrass <magrass@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/14 15:41:54 by magrass           #+#    #+#             */
+/*   Updated: 2026/04/14 15:44:09 by magrass          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+int	memcmp(const void *s1, const void *s2, size_t n);
+
+int	memcmp(const void *s1, const void *s2, size_t n)
+{
+	if (!n)
+		return (0);
+	
+}
