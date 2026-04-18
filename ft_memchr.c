09d238f4d131259cc1e0917ed07b69e9a5d6494f
may <may@urmom>
2026-04-18 13:54:46 +0200

    fixed the function name for ft_memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -12,9 +12,9 @@
 
 #include <stddef.h>
 
-void	*memchr(const void *s, int c, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 
-void	*memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t	i;
 
