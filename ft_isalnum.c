13687db00fbc509b4dc307ebd4fbf7fce173a94a
may <may@urmom>
2026-04-19 03:22:28 +0200

    fix(isalpha, isalnum): remove accidental inclusion of unwanted characters

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -14,5 +14,6 @@ int	ft_isalnum(int c);
 
 int	ft_isalnum(int c)
 {
-	return ((c >= 'A' && c <= 'z') || (c >= '0' && c <= '9'));
+	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')
+		|| (c >= '0' && c <= '9'));
 }
