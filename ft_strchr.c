cf4321862af25aa5249f24eb24c353da3661e0f4
may <may@urmom>
2026-04-20 15:56:21 +0200

    added testing to ft_strchr and made sure that it's being cast to unsigned char, according to man page

--- ft_strchr.c
+++ ft_strchr.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include <stddef.h>
+#include "libft.h"
 
 char	*ft_strchr(const char *s, int c);
 
@@ -21,11 +22,49 @@ char	*ft_strchr(const char *s, int c)
 	i = 0;
 	while (s[i])
 	{
-		if ((int)s[i] == c)
+		if ((unsigned char)s[i] == (unsigned char)c)
 			return ((char *)&s[i]);
 		i++;
 	}
-	if (s[i] == '\0')
-		return ((char *)s);
+	if ((unsigned char)c == '\0')
+		return ((char *)&s[i]);
 	return (NULL);
 }
+
+#ifdef TESTING
+
+typedef struct s_test {
+	char	*s;
+	int		c;
+}	t_test;
+
+static bool	run_test(t_test t)
+{
+	return (ft_strchr(t.s, t.c) == strchr(t.s, t.c));
+}
+
+int	main(void)
+{
+	static t_test	tests[] = {
+	{"hello", 'l'}, {"hello", 'h'}, {"hello", 'z'},
+	{"hello", '\0'}, {"", '\0'}, {"abcabc", 'b'}};
+	size_t			count;
+	size_t			passed;
+	size_t			i;
+	bool			ok;
+
+	count = sizeof(tests) / sizeof(t_test);
+	passed = 0;
+	i = 0;
+	while (i < count)
+	{
+		ok = run_test(tests[i]);
+		ft_print_line(i, count, ok);
+		passed += ok;
+		i++;
+	}
+	ft_print_summary("ft_strchr", passed, count);
+	return (passed != count);
+}
+
+#endif
