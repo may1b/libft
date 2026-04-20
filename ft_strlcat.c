b60a9f7a07456693e9d8afc1d661a600de0d803e
may <may@urmom>
2026-04-20 16:05:52 +0200

    fix(ft_strlcat): Added testing and then fixed the tests which were not passing by modifing the logic

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -17,26 +17,74 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
-	size_t	j;
-	size_t	src_len;
 	size_t	dst_len;
+	size_t	src_len;
 
-	j = 0;
-	dst_len = ft_strlen(dst);
-	src_len = ft_strlen(src);
-	i = dst_len;
-	if (size == 0)
-		return (src_len);
-	if (size < dst_len)
+	dst_len = 0;
+	while (dst[dst_len])
+		dst_len++;
+	src_len = 0;
+	while (src[src_len])
+		src_len++;
+	if (size <= dst_len)
 		return (src_len + size);
-	else
+	i = 0;
+	while (i < size - dst_len - 1 && src[i])
 	{
-		while (src[j] && (dst_len + j) < size)
-			dst[i++] = src[j++];
-		if ((dst_len + j) == size && dst_len < size)
-			dst[--i] = '\0';
-		else
-			dst[i] = '\0';
-		return (src_len + dst_len);
+		dst[dst_len + i] = src[i];
+		i++;
 	}
+	dst[dst_len + i] = '\0';
+	return (dst_len + src_len);
 }
+
+#ifdef TESTING
+
+typedef struct s_test {
+	char	dst_init[32];
+	char	src[32];
+	size_t	size;
+}	t_test;
+
+static bool	run_test(t_test t)
+{
+	char	fd[64];
+	char	ld[64];
+	size_t	fr;
+	size_t	lr;
+
+	memset(fd, 0xCC, 64);
+	memset(ld, 0xCC, 64);
+	strcpy(fd, t.dst_init);
+	strcpy(ld, t.dst_init);
+	fr = ft_strlcat(fd, t.src, t.size);
+	lr = strlcat(ld, t.src, t.size);
+	return (fr == lr && memcmp(fd, ld, 64) == 0);
+}
+
+int	main(void)
+{
+	static t_test	tests[] = {
+	{{"Hello"}, {" World"}, 20}, {{"Hello"}, {" World"}, 8},
+	{{"Hello"}, {" World"}, 0}, {{"Hello"}, {" World"}, 5},
+	{{""}, {"Hello"}, 10}};
+	size_t			count;
+	size_t			passed;
+	size_t			i;
+	bool			ok;
+
+	count = sizeof(tests) / sizeof(t_test);
+	passed = 0;
+	i = 0;
+	while (i < count)
+	{
+		ok = run_test(tests[i]);
+		ft_print_line(i, count, ok);
+		passed += ok;
+		i++;
+	}
+	ft_print_summary("ft_strlcat", passed, count);
+	return (passed != count);
+}
+
+#endif
