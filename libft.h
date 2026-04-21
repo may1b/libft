df708e992daa057cabd33a1bcae1ed10b4b99f09
May <magrass@student.42heilbronn.de>
2026-04-21 18:16:30 +0200

    done ft_lstdelone!

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: magrass <magrass@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/18 12:56:24 by magrass           #+#    #+#             */
-/*   Updated: 2026/04/21 18:11:55 by magrass          ###   ########.fr       */
+/*   Updated: 2026/04/21 18:16:18 by magrass          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -114,6 +114,8 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 t_list	*ft_lstlast(t_list *lst);
 
 void	ft_lstadd_back(t_list **lst, t_list *new);
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
 
 # ifdef TESTING
 
