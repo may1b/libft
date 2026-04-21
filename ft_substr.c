3d52010e4986b0fc570514a9f60f7d828f805d15
may <may@urmom>
2026-04-21 22:09:08 +0200

    added tesing to ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -28,3 +28,55 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	ft_memcpy(substr, &s[start], new_strlen);
 	return (substr);
 }
+
+#ifdef TESTING
+
+typedef struct s_test {
+	const char		*s;
+	unsigned int	start;
+	size_t			len;
+	const char		*expected;
+}	t_test;
+
+static bool	run_test(t_test t)
+{
+	char	*result;
+	bool	ok;
+
+	result = ft_substr(t.s, t.start, t.len);
+	if (!result)
+		return (false);
+	ok = (strcmp(result, t.expected) == 0);
+	free(result);
+	return (ok);
+}
+
+int	main(void)
+{
+	static t_test	tests[] = {
+	{"Hello World", 6, 5, "World"},
+	{"Hello", 10, 5, ""},
+	{"Hello", 0, 0, ""},
+	{"Hello", 1, 100, "ello"},
+	{"", 0, 5, ""},
+	{"Hello", 0, 5, "Hello"}};
+	size_t			count;
+	size_t			passed;
+	size_t			i;
+	bool			ok;
+
+	count = sizeof(tests) / sizeof(t_test);
+	passed = 0;
+	i = 0;
+	while (i < count)
+	{
+		ok = run_test(tests[i]);
+		ft_print_line(i, count, ok);
+		passed += ok;
+		i++;
+	}
+	ft_print_summary("ft_substr", passed, count);
+	return (passed != count);
+}
+
+#endif
