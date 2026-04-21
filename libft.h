54261dc32b736c0cdc80666ba803a9ac90d16944
May <magrass@student.42heilbronn.de>
2026-04-21 18:00:53 +0200

    done ft_lstsize, recursion AND correct first try i really am cooking rn, so much the french can already feel the 🔥🔥HEAT🔥🔥, they getting cooked, de frenchies

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: magrass <magrass@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/18 12:56:24 by magrass           #+#    #+#             */
-/*   Updated: 2026/04/21 17:35:55 by magrass          ###   ########.fr       */
+/*   Updated: 2026/04/21 17:52:56 by magrass          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -106,6 +106,8 @@ typedef struct s_list {
 }	t_list;
 
 t_list	*ft_lstnew(void *content);
+
+int		ft_lstsize(t_list *lst);
 
 void	ft_lstadd_front(t_list **lst, t_list *new);
 
