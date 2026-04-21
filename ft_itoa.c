27579e73010c9e8ae16bd60a14bae80d6e95e0a5
may <may@urmom>
2026-04-21 21:46:48 +0200

    added testing to ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -52,3 +52,50 @@ char	*ft_itoa(int n)
 	}
 	return (str);
 }
+
+#ifdef TESTING
+
+typedef struct s_test {
+	int			n;
+	const char	*expected;
+}	t_test;
+
+static bool	run_test(t_test t)
+{
+	char	*result;
+	bool	ok;
+
+	result = ft_itoa(t.n);
+	if (!result)
+		return (false);
+	ok = (strcmp(result, t.expected) == 0);
+	free(result);
+	return (ok);
+}
+
+int	main(void)
+{
+	static t_test	tests[] = {
+	{0, "0"}, {1, "1"}, {-1, "-1"},
+	{123, "123"}, {-456, "-456"},
+	{2147483647, "2147483647"}, {-2147483648, "-2147483648"}};
+	size_t			count;
+	size_t			passed;
+	size_t			i;
+	bool			ok;
+
+	count = sizeof(tests) / sizeof(t_test);
+	passed = 0;
+	i = 0;
+	while (i < count)
+	{
+		ok = run_test(tests[i]);
+		ft_print_line(i, count, ok);
+		passed += ok;
+		i++;
+	}
+	ft_print_summary("ft_itoa", passed, count);
+	return (passed != count);
+}
+
+#endif
