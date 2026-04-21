b6fba8bff17bb45625de0183fe390007b0e0c6d5
may <may@urmom>
2026-04-21 14:29:09 +0200

    done ft_substr!

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: magrass <magrass@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 20:29:58 by magrass           #+#    #+#             */
-/*   Updated: 2026/04/20 20:36:39 by magrass          ###   ########.fr       */
+/*   Updated: 2026/04/21 14:28:52 by magrass          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (ft_strlen(s) < start)
 		return (ft_calloc(1, 1));
 	new_strlen = ft_strlen(&s[start]);
-	
+	if (len < new_strlen)
+		new_strlen = len;
+	substr = ft_calloc(new_strlen + 1, 1);
+	if (!substr)
+		return (NULL);
+	ft_memcpy(substr, &s[start], new_strlen);
+	return (substr);
 }
