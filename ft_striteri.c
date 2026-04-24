f06cba1197f1fc640da246ded546d9aaeb2276a1
May <magrass@student.42heilbronn.de>
2026-04-24 19:09:24 +0200

    added additional protection to ft_striteri

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: magrass <magrass@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 21:44:42 by magrass           #+#    #+#             */
-/*   Updated: 2026/04/20 21:47:29 by magrass          ###   ########.fr       */
+/*   Updated: 2026/04/24 19:08:53 by magrass          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 	size_t	i;
 
 	i = 0;
+	if (!f || !s)
+		return ;
 	while (s[i])
 	{
 		f(i, &s[i]);
