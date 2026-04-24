76219ee118c629e067145b33d71021dc6bdf978d
May <magrass@student.42heilbronn.de>
2026-04-24 19:07:41 +0200

    added additonal protection to ft_lstiter

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: magrass <magrass@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 18:39:02 by magrass           #+#    #+#             */
-/*   Updated: 2026/04/21 18:45:36 by magrass          ###   ########.fr       */
+/*   Updated: 2026/04/24 19:06:48 by magrass          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	if (!lst)
+	if (!lst || !f)
 		return ;
 	f(lst->content);
 	ft_lstiter(lst->next, f);
