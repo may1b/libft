01cdd3d560cb13305e420d046169384a71d5fa3b
May <magrass@student.42heilbronn.de>
2026-04-24 19:11:53 +0200

    added additional protection to ft_putstr_fd

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: magrass <magrass@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 15:35:09 by magrass           #+#    #+#             */
-/*   Updated: 2026/04/21 15:36:55 by magrass          ###   ########.fr       */
+/*   Updated: 2026/04/24 19:11:15 by magrass          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	ft_putstr_fd(char *s, int fd)
 {
 	size_t	s_size;
 
+	if (!s)
+		return ;
 	s_size = ft_strlen(s);
 	write(fd, s, s_size);
 }
