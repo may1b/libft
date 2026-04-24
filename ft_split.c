d99d5a6a5dcbfa4bcfa6921cbb2090082e6722d5
may <may@urmom>
2026-04-24 15:41:53 +0200

    simplified libft by a lot and made sure it no longer does the very weird thing where it actually internally splits by a string instead of a char

--- ft_split.c
+++ ft_split.c
@@ -12,29 +12,7 @@
 
 #include "libft.h"
 
-/*
-Code which i once wrote:
-Not sure why i used uint16_t instead of size_t
-ArrayListSV splitSVByChar(SV sv, char splitBy)
-{
-    ArrayListSV svList = $initArrayList(SV);
-    uint16_t prevSplit = 0;
-    for (uint16_t i = 0; i < sv.len; ++i) {
-        if (sv.items[i] == splitBy) {
-            SV slicedSv = svSlice(sv, prevSplit, i - prevSplit);
-            $append((&svList), slicedSv);
-            prevSplit = i + 1;
-        }
-    }
-    if (prevSplit < sv.len) {
-        SV slicedSv = svSlice(sv, prevSplit, sv.len - prevSplit);
-        $append((&svList), slicedSv);
-    }
-    return svList;
-}
-*/
-
-static size_t	count_toks(char *s, char *cs, size_t cs_len)
+static size_t	count_toks(char const *s, char c)
 {
 	size_t	n;
 	size_t	i;
@@ -43,61 +21,50 @@ static size_t	count_toks(char *s, char *cs, size_t cs_len)
 	i = 0;
 	while (s[i])
 	{
-		while (s[i] && ft_strncmp(&s[i], cs, cs_len) == 0)
-			i += cs_len;
+		while (s[i] == c)
+			i++;
 		if (s[i])
 		{
 			n++;
-			while (s[i] && ft_strncmp(&s[i], cs, cs_len) != 0)
+			while (s[i] && s[i] != c)
 				i++;
 		}
 	}
 	return (n);
 }
 
-static char	*next_token(char *s, char *cs, size_t cs_len, size_t *pos)
+static char	*next_token(char const *s, char c, size_t *pos)
 {
 	size_t	start;
 
-	while (s[*pos] && ft_strncmp(&s[*pos], cs, cs_len) == 0)
-		*pos += cs_len;
+	while (s[*pos] == c)
+		(*pos)++;
 	if (!s[*pos])
 		return (NULL);
 	start = *pos;
-	while (s[*pos] && ft_strncmp(&s[*pos], cs, cs_len) != 0)
+	while (s[*pos] && s[*pos] != c)
 		(*pos)++;
 	return (ft_substr(s, start, *pos - start));
 }
 
-static char	**ft_split_by(char *str, char *charset)
+char	**ft_split(char const *s, char c)
 {
-	size_t	cs_len;
 	char	**result;
 	size_t	i;
 	size_t	tok_i;
 
-	cs_len = ft_strlen(charset);
-	result = malloc((count_toks(str, charset, cs_len) + 1) * sizeof(char *));
+	result = malloc((count_toks(s, c) + 1) * sizeof(char *));
 	if (!result)
 		return (NULL);
 	i = 0;
 	tok_i = 0;
-	result[tok_i] = next_token(str, charset, cs_len, &i);
+	result[tok_i] = next_token(s, c, &i);
 	while (result[tok_i])
 	{
 		tok_i++;
-		result[tok_i] = next_token(str, charset, cs_len, &i);
+		result[tok_i] = next_token(s, c, &i);
 	}
 	return (result);
-}
-
-char	**ft_split(char const *s, char c)
-{
-	char	delim[2];
-
-	delim[0] = c;
-	delim[1] = '\0';
-	return (ft_split_by((char *)s, delim));
 }
 
 #ifdef TESTING
